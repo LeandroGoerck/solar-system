fe4bdc4365b71c9691292ad580b64631370aa7e9
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-10-29 18:31:56 -0300

    concluido requisito 6 - add PlanetList to SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets.map((planet) => (
+            <PlanetCard
+              key={ planet.name }
+              planetImage={ planet.image }
+              planetName={ planet.name }
+            />
+          ))}
+        </ul>
       </div>
     );
   }
