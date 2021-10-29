1b570cb8d746a33e50d60c87d766f77f201fb2f8
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-10-29 15:36:43 -0300

    finalizado primeiro requisito - Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar Maravilhoso</p>);
+    return (
+      <>
+        <p>Sistema Solar Maravilhoso</p>
+        <Header />
+      </>
+    );
   }
 }
 
